1722af2fb9adb4566206eeca6f6a1b5250c3e4ac
Andy Wooten <andrewdwooten@icloud.com>
2017-01-18 14:27:29 -0700

    Extension complete: limits guesses and generates new secret number at limit.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(99)
+@@guess_count = 0
+@@x = rand(100)
 get '/' do
   guess = params["guess"]
-  message = check_guess(guess)
-  erb :index, :locals => {:x => SECRET_NUMBER, :message => message}
+  message = check_guess(guess,@@x)
+  erb :index, :locals => {:x => @@x, :message => message}
   
 end
 
-def check_guess(guess)
+def check_guess(guess,x)
   case 
-    when guess.to_i > SECRET_NUMBER + 5
-      "Way too high!!"
-    when guess.to_i < SECRET_NUMBER - 5
-      "Way too low!!"
-    when guess.to_i > SECRET_NUMBER
-      'Too high!!'
-    when guess.to_i < SECRET_NUMBER
-      "Too low!!"
-    when guess.to_i == SECRET_NUMBER
+    when guess.to_i > x + 5 && @@guess_count <= 5
+      @@guess_count += 1; "Way too high!!"
+    when guess.to_i < x - 5 && @@guess_count <= 5
+      @@guess_count += 1; "Way too low!!"
+    when guess.to_i > x && @@guess_count <= 5
+      @@guess_count += 1; 'Too high!!' 
+    when guess.to_i < x && @@guess_count <= 5
+      @@guess_count += 1;   "Too low!!"
+    when guess.to_i == x && @@guess_count <= 5
       "You got it right!\n
-        The SECRET NUMBER is #{SECRET_NUMBER}"
+        The SECRET NUMBER is #{x}"
+    when @@guess_count > 5
+      @@guess_count = 0; @@x = rand(100); 'New Secret Number!!'
     end
 end
